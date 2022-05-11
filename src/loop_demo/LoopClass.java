88c3585c55bd62d55bd8e38f15ae1ea35a4d277a
ATS <ravind.java2016@gmail.com>
2022-05-11 08:05:42 +0530

    Initial commit if condition

---
+++ src/loop_demo/LoopClass.java
@@ -0,0 +1,20 @@
+package loop_demo;
+
+public class LoopClass {
+    public static void main(String[] args){
+        for (int j=0;j<10;++j){
+            System.out.println("this is loop methode"+j);
+
+        }
+        for (int j=10;j>0;--j) {
+            System.out.println("this is decreege loops methode" + j);
+        }
+
+        int a=2;
+
+        for (int j=1;j<=10;j++){
+            System.out.println(a+"*"+j+"="+(a*j));
+
+        }
+    }
+}
---
+++ src/loop_demo/SecondClass.java
@@ -0,0 +1,12 @@
+package loop_demo;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+
+        for (int i=1;i<=10;i++){
+
+            System.out.println("my name is khushboo---"+i);
+        }
+    }
+}
